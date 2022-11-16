09c02e115d9e4235d4f7516501648f7560310f03
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-16 22:13:19 +0000

    My own assert function to test out code

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let strToDisplay = "";
+  if (actual === expected) {
+    strToDisplay = "✅✅✅ Assertion Passed: " + actual + " === " + expected;
+  } else {
+    strToDisplay = "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
+  }
+  console.log(strToDisplay);
+};
+
+//TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 10);
+
