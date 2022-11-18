e9ab935da35cd1ecce9dd3f99ee5e04d2f9409fe
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-18 19:05:37 +0000

    Added eqArrays function which compares two arrays and returns true if they are the same and false otherwise

---
+++ eqArrays.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  let strToDisplay = "";
+  if (actual === expected) {
+    strToDisplay = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    strToDisplay = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(strToDisplay);
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+//TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
+
