aa4f2c0a37cd6cd796e674cfd7ac6d68cfe317f2
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-18 21:28:44 +0000

    Added flatten function which takes an array with array elements and 'flattens' it to an array with no nested elements

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  let strToDisplay = "";
+  if (eqArrays(array1, array2)) {
+    strToDisplay = `✅✅✅ Assertion Passed: ${array1} === ${array2}`;
+  } else {
+    strToDisplay = `🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`;
+  }
+  console.log(strToDisplay);
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+//TEST CODE
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
