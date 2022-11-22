556321d94d0c3de23a092a5785b40c1af63e51dd
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-22 22:21:23 +0000

    First commit of findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  let strToDisplay = "";
+  if (actual === expected) {
+    strToDisplay = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    strToDisplay = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(strToDisplay);
+};
+
+const findKeyByValue = function(object, value) {
+  const keysArr = Object.keys(object);
+  for (let key of keysArr) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined);
