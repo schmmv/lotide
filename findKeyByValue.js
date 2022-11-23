43a128043dfeca3d4af7443f9c9fadcdf26bc041
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-23 01:09:43 +0000

    Added a few more test statements

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -26,3 +26,5 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
