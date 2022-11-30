340240e36222c317e8ba97008284e9f6b1e75e2b
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-11-30 15:53:05 +0000

    Shortened the without function by using filter array method. tested and passed.

--- without.js
+++ without.js
@@ -20,23 +20,30 @@ const assertArraysEqual = function(array1, array2) {
   console.log(strToDisplay);
 };
 
+// const without = function(source, itemsToRemove) {
+//   let arrWithout = [];
+//   let found = false;
+//   for (let i = 0; i < source.length; i++) {
+//     let check = source[i];
+//     for (let elem of itemsToRemove) {
+//       if (check === elem) {
+//         found = true;
+//       }
+//     }
+//     if (!found) {
+//       arrWithout.push(check);
+//     }
+//     found = false;
+//   }
+//   return arrWithout;
+// };
+
 const without = function(source, itemsToRemove) {
-  let arrWithout = [];
-  let found = false;
-  for (let i = 0; i < source.length; i++) {
-    let check = source[i];
-    for (let elem of itemsToRemove) {
-      if (check === elem) {
-        found = true;
-      }
-    }
-    if (!found) {
-      arrWithout.push(check);
-    }
-    found = false;
-  }
+  const arrWithout = source.filter((item) => !itemsToRemove.includes(item));
+  console.log(arrWithout);
   return arrWithout;
 };
+
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);// => [2, 3]
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => ["1", "2"]
