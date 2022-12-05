218053cec4eb5c42ec7fd4d2bb24706365425970
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-05 21:06:54 +0000

    Updated tests to use Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,32 @@
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
 const tail = require('../tail');
 
-//TEST CODE
-const test1 = tail(["Yo Yo", "Lighthouse", "Labs"]);
-assertEqual(test1.length, 2);
-assertEqual(test1[0], "Lighthouse");
-assertEqual(test1[1], "Labs");
+describe('#tail', () => {
+  it("returns ['Lighthouse', 'Labs'] for tail(['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns an array length of zero for an array of 1 element", () => {
+    assert.deepEqual(tail([1]).length, 0);
+  });
+  it("returns an empty array for an array of 1 element", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns an empty array for being given an empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
 
-const test2 = tail([1]);
-assertEqual(test2.length, 0);
-assertEqual(test2[0], undefined);
 
-const test3 = tail([]);
-assertEqual(test3.length, 0);
-assertEqual(test3[0], undefined);
+// //TEST CODE
+// const test1 = tail(["Yo Yo", "Lighthouse", "Labs"]);
+// assertEqual(test1.length, 2);
+// assertEqual(test1[0], "Lighthouse");
+// assertEqual(test1[1], "Labs");
+
+// const test2 = tail([1]);
+// assertEqual(test2.length, 0);
+// assertEqual(test2[0], undefined);
+
+// const test3 = tail([]);
+// assertEqual(test3.length, 0);
+// assertEqual(test3[0], undefined);
