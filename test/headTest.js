a8783834d5fb22ff1edd95877714a9c7b7323d50
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-05 20:46:39 +0000

    npm init the lotide folder and updated headTest.js to use Mocha and Chai for testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,22 @@
-const assertEqual = require('../assertEqual');
+const { assert } = require('chai');
+// const assertEqual = require('../assertEqual');
 const head = require('../head');
 
+describe('#head', () => {
+  it("return 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
+/*
 //TEST CODE
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
+*/
