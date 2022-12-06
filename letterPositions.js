b20c145702dad0f0965864abf2bf3def41c22453
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-06 03:09:05 +0000

    Moved test code to a test file

--- letterPositions.js
+++ letterPositions.js
@@ -1,8 +1,5 @@
 //Function which will return all the indices (zero-based positions), as an array,
 //in the string where each character is found
-//eqArrays and assertArraysEqual used to confirm results are correct
-const assertArraysEqual = require('./assertArraysEqual');
-
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -20,17 +17,6 @@ const letterPositions = function(sentence) {
 
 module.exports = letterPositions;
 
-const result1 = letterPositions("lighthouse in the house");
-console.log(result1);
-assertArraysEqual(result1["l"], [0]);
-assertArraysEqual(result1["i"], [1, 11]);
-assertArraysEqual(result1["g"], [2]);
-assertArraysEqual(result1["h"], [3, 5, 15, 18]);
-assertArraysEqual(result1["t"], [4, 14]);
-assertArraysEqual(result1["o"], [6, 19]);
-assertArraysEqual(result1["u"], [7, 20]);
-assertArraysEqual(result1["s"], [8, 21]);
-assertArraysEqual(result1["e"], [9, 16, 22]);
-assertArraysEqual(result1["n"], [12]);
+
 
 
