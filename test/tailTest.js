b4e4981e2fb9897dae22562190c6fcadfc248161
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-06 01:42:02 +0000

    Deleted old test code

--- test/tailTest.js
+++ test/tailTest.js
@@ -16,17 +16,3 @@ describe('#tail', () => {
   });
 });
 
-
-// //TEST CODE
-// const test1 = tail(["Yo Yo", "Lighthouse", "Labs"]);
-// assertEqual(test1.length, 2);
-// assertEqual(test1[0], "Lighthouse");
-// assertEqual(test1[1], "Labs");
-
-// const test2 = tail([1]);
-// assertEqual(test2.length, 0);
-// assertEqual(test2[0], undefined);
-
-// const test3 = tail([]);
-// assertEqual(test3.length, 0);
-// assertEqual(test3[0], undefined);
