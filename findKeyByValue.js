67ba9f37e4c4c9e223dd685d3f3d6541e10ed6a9
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-06 03:18:59 +0000

    Moved test code to test file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,8 +1,5 @@
-const assertEqual = require('./assertEqual');
-
 const findKeyByValue = function(object, value) {
-  const keysArr = Object.keys(object);
-  for (let key of keysArr) {
+  for (let key of Object.keys(object)) {
     if (object[key] === value) {
       return key;
     }
@@ -12,13 +9,3 @@ const findKeyByValue = function(object, value) {
 
 module.exports = findKeyByValue;
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That 70's Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
