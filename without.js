7151750e46a8cfcfb587e709250c3ffb941959a4
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-09 18:05:09 +0000

    Removed a console.log()

--- without.js
+++ without.js
@@ -1,6 +1,5 @@
 const without = function(source, itemsToRemove) {
   const arrWithout = source.filter((item) => !itemsToRemove.includes(item));
-  console.log(arrWithout);
   return arrWithout;
 };
 
